35c31afb6d38f27bd0552dcaae62359593fca1f9
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2023-10-14 13:04:15 +0500

    Fonts loaded

--- App.js
+++ App.js
@@ -3,28 +3,27 @@ import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Login, OTPScreen, Onboarding, Signup } from './src/screens';
+import { Login, LoginAuthentication, Onboarding, Signup } from './src/screens';
 
 const Stack = createStackNavigator();
+// SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    PoppinsBlack: require('./assets/fonts/Poppins-Black.ttf'),
-    'PoppinsBlackItalic': require('./assets/fonts/Poppins-BlackItalic.ttf'),
-    'PoppinsSemiBoldItalic': require('./assets/fonts/Poppins-SemiBoldItalic.ttf'),
-    'PoppinsExtraBold': require('./assets/fonts/Poppins-ExtraBold.ttf'),
-    'PoppinsExtraLight': require('./assets/fonts/Poppins-ExtraLight.ttf'),
-    'PoppinsItalic': require('./assets/fonts/Poppins-Italic.ttf'),
-    'PoppinsLight': require('./assets/fonts/Poppins-Light.ttf'),
-    'PoppinsMedium': require('./assets/fonts/Poppins-Medium.ttf'),
-    'PoppinsRegular': require('./assets/fonts/Poppins-Regular.ttf'),
-    'PoppinsSemiBold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+    // PoppinsBlack: require('./assets/fonts/Poppins-Black.ttf'),
+    // PoppinsExtraBold: require('./assets/fonts/Poppins-ExtraBold.ttf'),
+    // PoppinsExtraLight: require('./assets/fonts/Poppins-ExtraLight.ttf'),
+    PoppinsLight: require('./assets/fonts/Poppins-Light.ttf'),
+    // PoppinsMedium: require('./assets/fonts/Poppins-Medium.ttf'),
+    PoppinsRegular: require('./assets/fonts/Poppins-Regular.ttf'),
+    PoppinsSemiBold: require('./assets/fonts/Poppins-SemiBold.ttf'),
   });
 
   const onLayoutRootView = useCallback(async () => {
+    console.log('Fonts Loading...');
     if (fontsLoaded) {
-      // Hide the splash screen if fonts are loaded.
       await SplashScreen.hideAsync();
+      console.log('Fonts Loaded');
     }
   }, [fontsLoaded]);
 
@@ -38,7 +37,7 @@ export default function App() {
         <Stack.Screen name="Signin" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="Onboarding" component={Onboarding} />
-        <Stack.Screen name="Authentication" component={OTPScreen} />
+        <Stack.Screen name="Authentication" component={LoginAuthentication} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,7 +1,6 @@
 import Login from "./Login";
 import LoginAuthentication from "./LoginAuthentication";
-import OTPScreen from "./OTPScreen";
 import Onboarding from "./Onboarding";
 import Signup from "./Signup";
 
-export { Onboarding, Login, OTPScreen, LoginAuthentication, Signup }
+export { Onboarding, Login, LoginAuthentication, Signup }
