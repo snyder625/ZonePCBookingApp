de2715ee6b43c7e1e10d0779a2376ed13097173f
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2023-11-05 13:54:27 +0500

    Map Screen Updated

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,12 +1,75 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ImageBackground, ScrollView, TouchableOpacity, Image } from 'react-native'
+
+const filters = ['Standard', 'VIP', 'Premium'];
+const options = [
+  {
+    icon: '',
+    option: 'Available',
+    color: '#4BB468'
+  },
+  {
+    icon: '',
+    option: 'Reserved',
+    color: '#D03131'
+  },
+  {
+    icon: '',
+    option: 'Selected',
+    color: '#FFFFFF'
+  }
+];
 
 const MapScreen = () => {
   return (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
+    <ImageBackground source={require('../../assets/images/Background.png')} style={styles.container} resizeMode="cover">
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{maxHeight: 50, marginVertical: 16, position: 'absolute', top: 25}}>
+        {
+          filters.map((item, index)=> {
+            return (
+              <TouchableOpacity key={index} style={{display: 'flex', paddingHorizontal: 16, paddingVertical: 6, backgroundColor: '#074644', marginRight: 6, borderRadius: 48, justifyContent: 'center'}}>
+                <Text style={{color: 'white', fontFamily: 'PoppinsLight'}}>{item}</Text>
+              </TouchableOpacity>
+            )
+          })
+        }
+      </ScrollView>
+
+      <Image source={require('../../assets/images/map.png')} style={{marginBottom: 16}}/>
+
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{maxHeight: 40, marginVertical: 16}}>
+        {
+          options.map((option, index)=> {
+            return (
+              <TouchableOpacity key={index} style={{paddingVertical: 6, marginHorizontal: 12}}>
+                <Text style={{color: option.color, fontFamily: 'PoppinsLight'}}>{option.option}</Text>
+              </TouchableOpacity>
+            )
+          })
+        }
+      </ScrollView>
+
+      <View style={{backgroundColor: '#074644', width: '80%', alignItems: 'center', gap: 8, paddingVertical: 16, elevation: 4, borderRadius: 16}}>
+        <Text style={{color: 'white', fontFamily: 'PoppinsMedium', fontSize: 18}}>Selected Seat - 14</Text>
+      </View>
+
+      <View style={{backgroundColor: '#074644', width: '80%', alignItems: 'center', gap: 8, paddingVertical: 16, elevation: 4, borderRadius: 16}}>
+        <Text style={{color: 'white', fontFamily: 'DMSansRegular', fontSize: 18}}>Standard Zone</Text>
+        <Text style={{color: '#4BB468', fontSize: 13, fontFamily: 'PoppinsRegular'}}>Available</Text>
+      </View>
+    </ImageBackground>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    backgroundColor: '#07161B',
+    position: 'relative',
+    gap: 20,
+    paddingBottom: 30
+  }
+})
 
 export default MapScreen
